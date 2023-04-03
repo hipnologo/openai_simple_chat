7f2593ccace9a0f6f673b86c3434811f8572280b
Fabio <email@fabioac.com>
2023-04-03 11:19:58 -0400

    Added streamlit version

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import openai
 import streamlit as st
 import os
 
+# Set the Model of use
+LAST_GPT = True
+
 # Set OpenAI API key
-if 'api_key' in globals():
+if 'api_key' not in globals():
     api_key = os.getenv("OPENAI_API_KEY")
     openai.api_key = api_key
 else:
@@ -13,34 +16,46 @@ else:
     except Exception as e:
         st.warning("No OpenAI API Key has been found")
 
-    # If no response with text is found, return the first response's content (which may be empty)
-    return response.choices[0].message.content
-  
 # Define the function to generate responses
-def generate_response(message_log):
-    # Use OpenAI's Completion API to get the chatbot's response
-    completions = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
-        prompt=message_log[-1]["content"],  # The last message in the conversation history as the prompt
-        #max_tokens=1024,  # The maximum number of tokens (words or subwords) in the generated response
-        #n=1,  # The number of responses to generate
-        #stop=None,  # The stopping sequence for the generated response, if any (not used here)
-        temperature=0.7,  # The "creativity" of the generated response (higher temperature = more creative)
-    )
-    
-    # Find the first response from the chatbot that has text in it (some responses may not have text)
-    for choice in completions.choices:
-        if "text" in choice:
-            return choice.text
+def generate_response(message_log, prompt):
+    if not message_log:
+        # Return a default message if the conversation history is empty
+        return "Hi, how can I help you today?"
 
-    # Get the first response from the chatbot that has text in it (some responses may not have text)
-    # Or, if no response with text is found, return the first response's content (which may be empty)
-    message = completions.choices[0].text.strip()
-    #message = completions.choices[0].message.content
+    try:
+        if LAST_GPT == True:
+            ### gpt-3.5-turbo model
+            messages = [dict(role="user", content=prompt)]
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages,  # The last message in the conversation history as the prompt
+                max_tokens=100,
+                n=1,
+                stop=None,
+                temperature=0.3, # The "creativity" of the generated response (higher temperature = more creative)
+                presence_penalty=2
+            )
+            message = response['choices'][0]['message']['content']
+        else:
+            ### text-davinci-003 model
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=prompt,
+                max_tokens=100,
+                n=1,
+                stop=None,
+                temperature=0.3,
+                presence_penalty=2
+            )
+            message = response.choices[0].text.strip()
 
-    # Add the chatbot's response to the conversation history
-    message_log.append({"role": "assistant", "content": message})
-    return message
+        # Add the chatbot's response to the conversation history
+        message_log.append({"role": "assistant", "content": message})
+        return message
+
+    except Exception as e:
+        st.warning(f"An error occurred while processing your request: {e}")
+        return None
 
 # Set up the Streamlit app
 st.title("Chat with an AI")
@@ -50,8 +65,9 @@ if 'message_log' not in st.session_state:
 # Get the user input and generate responses
 user_input = st.text_input("You:")
 if user_input:
+    prompt = st.session_state['message_log'][-1]["content"] if st.session_state['message_log'] else ""
     st.session_state['message_log'].append({"role": "user", "content": user_input})
-    ai_response = generate_response(st.session_state['message_log'])
+    ai_response = generate_response(st.session_state['message_log'], prompt)
 
 # Display the conversation history
 if st.session_state['message_log']:
